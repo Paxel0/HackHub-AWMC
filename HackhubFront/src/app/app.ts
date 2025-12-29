ceb72adf6baf02f6808e05a65d6c1e06d597980b
Mariagrazia Commone <mariagrazia.commone@studenti.unicam.it>
2025-12-29 09:53:12 +0100

    navbar e footer

--- HackhubFront/src/app/app.ts
+++ HackhubFront/src/app/app.ts
@@ -1,11 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
-
-
+import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref, HttpClientModule],
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    RouterLinkWithHref, 
+    HttpClientModule, 
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
