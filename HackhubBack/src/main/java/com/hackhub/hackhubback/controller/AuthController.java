acb67a5d3003f1058438262a6a47e07032b3e1f0
Nicola <nicola.lobascio@studenti.unicam.it>
2025-12-30 17:10:55 +0100

    primi controller e service

---
+++ HackhubBack/src/main/java/com/hackhub/hackhubback/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.hackhub.hackhubback.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hackhub.hackhubback.DTO.LoginRequest;
+import com.hackhub.hackhubback.entity.User;
+import com.hackhub.hackhubback.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class AuthController {
+    @Autowired
+private UserService userService;
+@Autowired
+private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
+        
+        User user = userService.findIdByUsername(loginRequest.getUsername());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Utente non trovato");
+        }
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Password errata");
+        }
+
+        return ResponseEntity.ok("Login effettuato con successo");
+    }
+    
+    
+}
---
+++ HackhubBack/src/main/java/com/hackhub/hackhubback/service/UserService.java
@@ -0,0 +1,19 @@
+package com.hackhub.hackhubback.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hackhub.hackhubback.entity.User;
+import com.hackhub.hackhubback.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findIdByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+    
+}
