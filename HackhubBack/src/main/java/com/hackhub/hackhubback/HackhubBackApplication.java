a7db8f1b6aa7588f4a474f0a6d28c66881acc412
Mariagrazia Commone <mariagrazia.commone@studenti.unicam.it>
2026-02-17 11:31:18 +0100

    containerizzazione 1

--- HackhubBack/src/main/java/com/hackhub/hackhubback/HackhubBackApplication.java
+++ HackhubBack/src/main/java/com/hackhub/hackhubback/HackhubBackApplication.java
@@ -3,6 +3,8 @@ package com.hackhub.hackhubback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class HackhubBackApplication {
 
