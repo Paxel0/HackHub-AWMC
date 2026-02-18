cac7621275fdca55ccd63c5523ede89db864a382
Giada Vecchi <giada.vecchi@studenti.unicam.it>
2026-02-18 11:37:02 +0100

    fix karma

--- HackhubFront/karma.conf.js
+++ HackhubFront/karma.conf.js
@@ -1,4 +1,4 @@
-// Karma configuration file, see link for more information
+// Karma configuration file
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
@@ -13,16 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/front-end'),
