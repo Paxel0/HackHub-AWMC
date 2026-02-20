aabd29815264bddb0483e1831fef2d573b8b7ead
Nicola <nicola.lobascio@studenti.unicam.it>
2026-02-20 11:19:15 +0100

    controllo presenza user admin

--- HackhubBack/src/main/java/com/hackhub/hackhubback/HackhubBackApplication.java
+++ HackhubBack/src/main/java/com/hackhub/hackhubback/HackhubBackApplication.java
@@ -1,8 +1,14 @@
 package com.hackhub.hackhubback;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.hackhub.hackhubback.entity.User;
+import com.hackhub.hackhubback.repository.UserRepository;
 
 
 
@@ -12,6 +18,17 @@ public class HackhubBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HackhubBackApplication.class, args);
 	}
-
+@Bean
+    CommandLineRunner initAdmin(UserRepository userRepo, PasswordEncoder encoder) {
+        return args -> {
+            if (userRepo.findByUsername("admin").isEmpty()) {
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setPassword(encoder.encode("admin123"));
+                admin.setRole("ADMIN");
+                userRepo.save(admin);
+            }
+        };
+    }
 }
 
