82d9ef8d8df93189e2517bdb9ae5222841f5bc37
Nicola <nicola.lobascio@studenti.unicam.it>
2026-02-20 09:03:09 +0100

    prova k8s 3

--- HackhubBack/src/main/java/com/hackhub/hackhubback/HackhubBackApplication.java
+++ HackhubBack/src/main/java/com/hackhub/hackhubback/HackhubBackApplication.java
@@ -1,19 +1,8 @@
 package com.hackhub.hackhubback;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.hackhub.hackhubback.entity.User;
-import com.hackhub.hackhubback.repository.UserRepository;
-
-
-
-
-
 
 @SpringBootApplication
 public class HackhubBackApplication {
